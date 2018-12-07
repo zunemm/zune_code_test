d51e2b6b0bc83939101b90245585826d41b56d6a
zunemm <junekoko@hotmail.com>
2018-12-07 20:55:56 +0630

    Update row_add.js

--- js/row_add.js
+++ js/row_add.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         cols += '<td><input type="text" class="form-control" name="qty' + counter + '" id="qty' + counter + '" value=""/></td>';
         cols += '<td><input type="text" class="form-control" name="price' + counter + '" id="price' + counter + '" value="" oninput="getTotal(' + counter + ')"/></td>';
 
-        cols += '<td><label id="total' + counter + '">total' + counter + '</label></td>';
+        cols += '<td><label id="total' + counter + '"></label></td>';
        
         /*cols += '<td><input type="button" class="ibtnDel btn btn-md btn-danger "  value="Delete"></td>';*/
         cols += '<td><a href=""><img src="images/trash-can-icon-png.png" style="width: 30px; height: 20px;"></a></td>';
